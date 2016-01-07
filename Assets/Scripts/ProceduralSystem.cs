d26bc36169a7507b25847a152cc3dfd65342fd1e
Emilian Manolache <e.manolache@dataclaritycorp.com>
2016-01-07 12:35:00 +0200

    Added extra SGT assets, added various planets to system, added proper sun light to planets

--- Assets/Scripts/ProceduralSystem.cs
+++ Assets/Scripts/ProceduralSystem.cs
@@ -7,25 +7,26 @@ public class ProceduralSystem : MonoBehaviour
 {
 	public Mesh SphereMesh;
 
-	public Light MainLight;
-
-	public List<Material> StarMaterials = new List<Material>();
-
 	public List<Material> PlanetMaterials = new List<Material>();
 
 	public List<Material> MoonMaterials = new List<Material>();
 
-	public List<Cubemap> JovianTextures = new List<Cubemap>();
-
 	public List<Object> GeneratedObjects = new List<Object>();
 
-    public Texture ProminenceTexture = new Texture();
 
-    public int moonsRange;
+    public List<GameObject> planetPrefabs = new List<GameObject>();
 
-    public GameObject starPrefab;
+    public List<GameObject> starPrefabs = new List<GameObject>();
 
-	public void Clear()
+    public GameObject starLightNormalPlanetPrefab;
+
+    public GameObject starLightGasPlanetPrefab;
+
+    public GameObject newStarNormalLight;
+
+    public GameObject newStarGasLight;
+
+    public void Clear()
 	{
 		for (var i = GeneratedObjects.Count - 1; i >= 0; i--)
 		{
@@ -37,43 +38,97 @@ public class ProceduralSystem : MonoBehaviour
 
 	public void AddStar()
 	{
-		var gameObject   = AddBasicGameObject("Star", transform, 0.0f, 0.0f, 0.0f, 10.0f, 3.0f, 3.0f);
-        
-        var newStar = (GameObject) Instantiate(starPrefab, new Vector3(0.0f, 0.0f, 0.0f), gameObject.transform.rotation);
+        if (starPrefabs.Count == 0) return;
         var solarSystem = GameObject.FindGameObjectsWithTag("SolarSystem")[0];
+        var placeholder   = AddBasicGameObject("Star", transform, 0.0f, 0.0f, 0.0f, 10.0f, 3.0f, 3.0f);
+        var newStar = (GameObject) Instantiate(starPrefabs[Random.Range(0, (starPrefabs.Count - 1))], new Vector3(0.0f, 0.0f, 0.0f), placeholder.transform.rotation);
+        
         newStar.transform.parent = solarSystem.transform;
-        newStar.transform.localScale = gameObject.transform.localScale;
+        newStar.transform.localScale = placeholder.transform.localScale;
         newStar.transform.localPosition = new Vector3(0.0f, -5.0f, 0.0f);
 
-        Destroy(gameObject);
+        newStarNormalLight = (GameObject)Instantiate(starLightNormalPlanetPrefab, new Vector3(0.0f, 0.0f, 0.0f), placeholder.transform.rotation);
+        newStarNormalLight.transform.parent = solarSystem.transform;
+        newStarNormalLight.transform.localPosition = new Vector3(0.0f, -20.0f, 0.0f);
+
+        newStarGasLight = (GameObject)Instantiate(starLightGasPlanetPrefab, new Vector3(0.0f, 0.0f, 0.0f), placeholder.transform.rotation);
+        newStarGasLight.transform.parent = solarSystem.transform;
+        newStarGasLight.transform.localPosition = new Vector3(0.0f, -20.0f, 0.0f);
+
+        Destroy(placeholder);
 
     }
 
 	public void AddPlanet()
 	{
-		var gameObject   = AddBasicGameObject("Planet", transform, 15.0f, 30.0f, -3.0f, 3.0f, 0.5f, 1.0f);
-		var meshFilter   = gameObject.AddComponent<MeshFilter>();
-		var meshRenderer = gameObject.AddComponent<MeshRenderer>();
-		var atmosphere   = gameObject.AddComponent<SgtAtmosphere>();
+        if (planetPrefabs.Count == 0) return;
 
-		meshFilter.sharedMesh = SphereMesh;
+        // place planet
+        var solarSystem = GameObject.FindGameObjectsWithTag("SolarSystem")[0];
+        var placeholder = AddBasicGameObject("Planet", transform, 15.0f, 30.0f, -3.0f, 3.0f, 0.5f, 1.0f);
+        var newPlanet = (GameObject)Instantiate(planetPrefabs[Random.Range(0, (planetPrefabs.Count - 1))], new Vector3(0.0f, 0.0f, 0.0f), placeholder.transform.rotation);
 
-		meshRenderer.sharedMaterial = GetRandomElement(PlanetMaterials);
+        // position within star system
+        newPlanet.transform.parent = solarSystem.transform;
+        newPlanet.transform.localScale = placeholder.transform.localScale;
+        newPlanet.transform.localPosition = new Vector3(0.0f, -5.0f, 0.0f);
 
-		atmosphere.InnerPower = Random.Range(1.0f, 2.0f);
+        // fix orbit and rotation
+        var simpleOrbit = newPlanet.AddComponent<SimpleOrbit>();
+        var rotate = newPlanet.AddComponent<SgtRotate>();
+        simpleOrbit.Angle = placeholder.GetComponent<SimpleOrbit>().Angle;
+        simpleOrbit.Radius = placeholder.GetComponent<SimpleOrbit>().Radius;
+        simpleOrbit.Center = new Vector3(placeholder.GetComponent<SimpleOrbit>().Center.x, placeholder.GetComponent<SimpleOrbit>().Center.y - 3, placeholder.GetComponent<SimpleOrbit>().Center.z);
+        simpleOrbit.DegreesPerSecond = placeholder.GetComponent<SimpleOrbit>().DegreesPerSecond;
+        rotate.DegreesPerSecond = placeholder.GetComponent<SgtRotate>().DegreesPerSecond;
 
-		atmosphere.Lights.Add(MainLight);
+        // add atmosphere lights
+        if ((newPlanet.GetComponent<SgtAtmosphere>() as SgtAtmosphere) != null)
+        {
+            newPlanet.GetComponent<SgtAtmosphere>().Lights[0] = newStarGasLight.GetComponent<Light>();
+        }
 
-		atmosphere.OuterMeshes.Add(SphereMesh);
+        // add cloudsphere lights
+        if ((newPlanet.GetComponent<SgtCloudsphere>() as SgtCloudsphere) != null)
+        {
+            newPlanet.GetComponent<SgtCloudsphere>().Lights[0] = newStarGasLight.GetComponent<Light>();
+        }
 
-		// Add moons?
-		/*for (var i = Random.Range(0, moonsRange); i >= 0; i--)
+        // add jovian lights
+        if ((newPlanet.GetComponent<SgtJovian>() as SgtJovian) != null)
+        {
+            newPlanet.GetComponent<SgtJovian>().Lights[0] = newStarGasLight.GetComponent<Light>();
+        }
+        else
+        {
+            foreach (Transform child in newPlanet.transform)
+            {
+                if ((child.gameObject.GetComponent<SgtJovian>() as SgtJovian) != null)
+                {
+                    child.gameObject.GetComponent<SgtJovian>().Lights[0] = newStarGasLight.GetComponent<Light>();
+                }
+            }
+        }
+
+        // add ring lights
+        foreach (Transform child in newPlanet.transform)
+        {
+            if ((child.gameObject.GetComponent<SgtRing>() as SgtRing) != null)
+            {
+                child.gameObject.GetComponent<SgtRing>().Lights[0] = newStarNormalLight.GetComponent<Light>();
+            }
+        }
+
+        Destroy(placeholder);
+
+        // Add moons?
+        /*for (var i = Random.Range(0, 2); i >= 0; i--)
 		{
 			AddMoon(gameObject.transform);
 		}*/
-	}
+    }
 
-	public void AddMoon(Transform parent)
+    public void AddMoon(Transform parent)
 	{
 		var gameObject   = AddBasicGameObject("Moon", parent, 1.0f, 3.0f, -30.0f, 30.0f, 0.05f, 0.2f);
 		var meshFilter   = gameObject.AddComponent<MeshFilter>();
@@ -83,24 +138,12 @@ public class ProceduralSystem : MonoBehaviour
 
 		meshRenderer.sharedMaterial = GetRandomElement(MoonMaterials);
 	}
-
-	public void AddJovian()
-	{
-		var gameObject = AddBasicGameObject("Jovian", transform, 5.0f, 30.0f, -5.0f, 10.0f, 0.5f, 1.0f);
-		var jovian     = gameObject.AddComponent<SgtJovian>();
-
-		jovian.MainTex = GetRandomElement(JovianTextures);
-
-		jovian.Lights.Add(MainLight);
-
-		jovian.Meshes.Add(SphereMesh);
-	}
-
+    
 	public GameObject AddBasicGameObject(string name, Transform parent, float minOrbitDistance, float maxOrbitDistance, float minRotationSpeed, float maxRotationSpeed, float minScale, float maxScale)
 	{
 		// Create GO
 		var gameObject  = new GameObject(name);
-		var simpleOrbit = gameObject.AddComponent<SgtSimpleOrbit>();
+		var simpleOrbit = gameObject.AddComponent<SimpleOrbit>();
 		var rotate      = gameObject.AddComponent<SgtRotate>();
 		var scale       = Random.Range(maxScale, maxScale);
 
@@ -132,14 +175,9 @@ public class ProceduralSystem : MonoBehaviour
 		{
 			AddStar();
 
-			for (var i = Random.Range(1, 6); i >= 0; i--)
+			for (var i = Random.Range(1, 12); i >= 0; i--)
 			{
 				AddPlanet();
-			}
-
-			for (var i = Random.Range(1, 4); i >= 0; i--)
-			{
-				AddJovian();
 			}
 		}
 	}
